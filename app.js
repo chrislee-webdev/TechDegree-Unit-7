5f94f433de2d108dc55ffe66a8352673ee541724
Christopher Lee <leechrs1@gmail.com>
2021-07-27 23:08:14 -0500

    Added app.js file
    added app.js file for alert banner and toggle settings but can't get it to be displayed for some reason.

---
+++ app.js
@@ -0,0 +1,9 @@
+/*Alert Banner*/
+
+const alertBanner = document.getElementById("alert");
+
+alertBanner.innerHTML = 
+    <div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>7</strong> overdue tasks</p>
+    <p class="alert-button-close">x</p>
+    </div>
