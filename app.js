6a97b4fee570e1290406de28dc51c0465b4f61c2
Christopher Lee <leechrs1@gmail.com>
2021-08-01 22:44:50 -0500

    Implemented Alert Banner

--- app.js
+++ app.js
@@ -3,7 +3,15 @@
 const alertBanner = document.getElementById("alert");
 
 alertBanner.innerHTML = 
+`
     <div class="alert-banner">
     <p><strong>Alert:</strong> You have <strong>7</strong> overdue tasks</p>
-    <p class="alert-button-close">x</p>
+    <p class="alert-banner-close">x</p>
     </div>
+`
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+});
