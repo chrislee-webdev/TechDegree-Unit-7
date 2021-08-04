e88f3ab51929c0ef40abdf3537bfef2dbb7e68ed
Christopher Lee <leechrs1@gmail.com>
2021-08-03 22:16:01 -0500

    Updated charts.js and app.js

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ alertBanner.addEventListener('click', e => {
         alertBanner.style.display = "none"
     }
 });
+
+/*Messaging Section*/
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+    if(user.value === "" & message.value === "" ) {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+        alert("Please fill out message field befire sending");
+    } else {
+        alert("Message successfuly sent to: ${user.value}");
+    }
+});
--- chart.js
+++ chart.js
@@ -1,6 +1,6 @@
 //Line Charts
 
-var points = document.getElementById('traffic-chart').getContext('2d');
+/* var points = document.getElementById('traffic-chart').getContext('2d');
                     var myChart = new Chart(points, {
                         type: 'line',
                         data: {
@@ -30,10 +30,47 @@ var points = document.getElementById('traffic-chart').getContext('2d');
                             }
                         }
                     });
+                    */
+
+var trafficCanvas = document.getElementById('traffic-chart').getContext('2d');
+                    let trafficData = {
+                        labels: ["12-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+                        datasets: [{
+                            data: [700, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+                            backgroundColor :"rgba(116, 119, 191, .3)",
+                            borderWidth: 1,
+                        }]
+                    };
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duartion: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let traffiChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+
+
 
 //Bar Chart
 
-var bars = document.getElementById('daily-chart').getContext('2d')
+/* var bars = document.getElementById('daily-chart').getContext('2d')
 var anotherCharts = new Chart(bars, {
     type: 'bar',
     data: {
@@ -67,9 +104,41 @@ var anotherCharts = new Chart(bars, {
     }
 });
 
+*/
+
+const dailyCanvas = document.getElementById('daily-chart').getContext('2d');
+            const dailyData = {
+            labels: ["S", "M", "T", "W", "T", "F", "S"],
+            datasets: [{
+            label: "# of hits",
+            data: [75, 115, 175, 125, 225, 200, 100],
+            backgroundColor: "#7477BF",
+            borderWidth: 1,
+        }]
+};
+
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: "bar",
+    data: dailyData,
+    options: dailyOptions
+});
+
 //Doughnut Chart
 
-    var pie = document.getElementById('mobile-chart').getContext('2d');
+   /*var pie = document.getElementById('mobile-chart').getContext('2d');
                     var myChart = new Chart(pie, {
                         type: 'doughnut',
                         data: {
@@ -97,22 +166,39 @@ var anotherCharts = new Chart(bars, {
                         }
                        
                     });
+                    */
 
+const mobileCanvas = document.getElementById("mobile-chart").getContext('2d');
 
-// Alert Banner
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: "# of users",
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            "#7477BF",
+            "#78CF82",
+            "#51B6C8"
+        ]
+    }]
+};
 
-/* const alertBanner = document.getElementById("alert");
-
-alertBanner.innerHTML = 
-    <div class="alert-banner">
-        <p><strong>Alert:</strong> You have <strong>5</strong> overdue tasks to complete</p>
-        <p class="alert-banner-close">x</p>
-    </div>
-
-alertBanner.addEventListener('click', e => {
-    const element = e.target;
-    if (element.classList.contains("alert-banner-close")) {
-        alertBanner.style.display = "none"
+const mobileOptions = {
+    plugins: {
+        legend: {
+            position: "right",
+            labels: {
+                boxWidth: 20,
+                fontStyle: "bold"
+            }
+            
+        }
     }
-}); */
-    
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: "doughnut",
+    data: mobileData,
+    options: mobileOptions
+});
