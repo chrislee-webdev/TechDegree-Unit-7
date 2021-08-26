4ceda310edd02c3deaafb3f315f1d721037c500c
Christopher Lee <leechrs1@gmail.com>
2021-08-25 22:53:46 -0500

    Added color to Traffic chart

--- chart.js
+++ chart.js
@@ -37,8 +37,11 @@ var trafficCanvas = document.getElementById('traffic-chart').getContext('2d');
                         labels: ["12-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
                         datasets: [{
                             data: [700, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-                            backgroundColor :"rgba(116, 119, 191, .3)",
+                            backgroundColor :"#7477BF",
                             borderWidth: 1,
+                            fill: {
+                                target: 'origin',                                
+                            }
                         }]
                     };
 
